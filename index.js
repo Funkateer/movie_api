5b293c54e270e9b98eef2d56417451d9c9dd58ca
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-04-02 00:19:26 +0200

    add '/' and '/movies' routes in index.js file

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const morgan = require('morgan');
 
 const app = express();
 
+// '/movies' route response, return movie data
+app.get('/movies',(req,res)=>
+ {
+  res.sendFile('topMovies.JSON', { root : __dirname })
+});
+
+// '/' aka home route response
+app.get('/', (req, res) => {
+  res.send('Welcome to my first RESTful API')
+});
+
+
 app.listen(8080, () =>{
   console.log('App listening on port 8080')
 });
