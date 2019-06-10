ab47ba11f6f0620b24e0cd29acb12e721adabda7
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-06-10 19:12:47 +0200

    add stylings to main-view and login-view

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import './login-view.scss';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -17,16 +22,30 @@ export function LoginView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="button" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Container className='login-view'>
+      <h1>Login</h1>
+      <Form>
+        <Form.Group controlId='formUsername'>
+          <Form.Label>Username:</Form.Label>
+          <Form.Control size='sm' type='text' placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} />
+        </Form.Group>
+        <Form.Group controlId='formPassword'>
+          <Form.Label>Password:</Form.Label>
+          <Form.Control size='sm' type='password' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
+        <Button variant='primary' onClick={handleSubmit} >Submit</Button>
+        <Form.Group controlId='formNewUser'>
+          <Form.Text>New user? Click <Button id='login-view__register' style={{ padding: 0 }} variant='link' onClick={() => props.NewUser()}> here </Button> to register</Form.Text>
+        </Form.Group>
+      </Form>
+    </Container>
   );
+}
+
+LoginView.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  NewUser: PropTypes.func.isRequired,
+  OnLoggedIn: PropTypes.func.isRequired
 }
--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -4,6 +4,11 @@ import axios from 'axios';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { RegistrationView } from '../registration-view/registration-view';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './main-view.scss';
 
 export class MainView extends React.Component {
 
@@ -42,31 +47,55 @@ export class MainView extends React.Component {
     });
   }
 
+  ResetMainView() {
+    this.setState({
+      selectedMovie: null
+    });
+  }
   onLoggedIn(user) {
     this.setState({
       user
     });
   }
 
+  RegisterUser() {
+    this.setState({
+      newUser: true
+    });
+  }
+
+  UserRegistered() {
+    this.setState({
+      newUser: null
+    });
+  }
+
   render() {
     // If the state isn't initialized, this will throw on runtime
     // before the data is initially loaded
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie,user, newUser } = this.state;
 
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    if (!user) {
+      if (newUser) return <RegistrationView UserRegistered={() => this.UserRegistered()} OnLoggedIn={user => this.OnLoggedIn(user)} />;
+      else return <LoginView OnLoggedIn={user => this.OnLoggedIn(user)} NewUser={() => this.RegisterUser()} UserRegistered={() => this.UserRegistered()} />;
+    }
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view"/>;
 
     return (
-    <div className="main-view">
-      {selectedMovie
-        ? <MovieView movie={selectedMovie}/>
-        : movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
-        ))
-      }
-     </div>
+      <Container className='main-view' fluid='true'>
+        <Row>
+          {selectedMovie
+            ? <Col><MovieView returnCallback={() => this.ResetMainView()} movie={selectedMovie} /></Col>
+            : movies.map(movie => {
+              return (
+                <Col xl={3} sm={6} md={4} xs={12}><MovieCard key={movie._id} movie={movie} onClick={movie => this.OnMovieClick(movie)} /></Col>
+              )
+            })
+          }
+        </Row>
+      </Container>
     );
   }
 
