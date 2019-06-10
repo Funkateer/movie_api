039d8cd1827251173cf14983d29a8513754e5081
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-06-10 19:27:23 +0200

    add stylings to movie-view

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MainView } from '../main-view/main-view';
+import Button from 'react-bootstrap/Button';
+import './movie-view.scss';
 
 export class MovieView extends React.Component {
 
@@ -13,26 +17,41 @@ export class MovieView extends React.Component {
     if (!movie) return null;
 
     return (
-      <div className="movie-view">
-      <div className="movie-title">
-        <div className="label">Title</div>
-        <div className="value">{movie.Title}</div>
-      </div>
-      <div className="movie-description">
-        <div className="label">Description</div>
-        <div className="value">{movie.Description}</div>
-      </div>
-      <img className="movie-poster" src={movie.ImagePath} />
-      <div className="movie-genre">
-        <div className="label">Genre</div>
-        <div className="value">{movie.Genre.Name}</div>
-      </div>
-      <div className="movie-director">
-        <div className="label">Director</div>
-        <div className="value">{movie.Director.Name}</div>
-      </div>
-      <button onClick={() => onClick()}>Back to all movies</button>
-      </div>
-    );
-  }
+      <div className='movie-view'>
+        <div className='movie-title'>
+          <h2 className='label'>Title</h2>
+          <p className='value'>{this.props.movie.Title}</p>
+        </div>
+        <div className='movie-description'>
+          <h3 className='label'>Description</h3>
+          <p className='value'>{this.props.movie.Description}</p>
+        </div>
+        <div className='movie-genre'>
+          <h3 className='label'>Genre</h3>
+          <p className='value'>{this.props.movie.Genre.Name}</p>
+        </div>
+        <div className='movie-director'>
+          <h3 className='label'>Director</h3>
+          <p className='value'>{this.props.movie.Director.Name}</p>
+        </div>
+        <div className='return-button'>
+          <Button variant='primary' onClick={() => this.props.returnCallback()}>Return</Button>
+        </div>
+      </div> // movie-view
+    );// return
+  } // render
+}
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+    Description: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string
+    })
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
 }
